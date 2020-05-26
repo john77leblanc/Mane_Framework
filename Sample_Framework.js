00fe89031a541b41dc5f3c10226abd20c229bb82
Johnathan LeBlanc <john.leblanc03@gmail.com>
2020-05-26 09:36:08 -0400

    Defined the framework. Event handler updated.

--- Sample_Framework.js
+++ Sample_Framework.js
@@ -1,28 +1,83 @@
 const Mane = () => {
+  //--------------------------------------------------
+  // Target element to render the app
+  //--------------------------------------------------
   this.target = () => document.querySelector("#app");
 
+  //--------------------------------------------------
+  // Required data for build.
+  // DO NOT TOUCH
+  //--------------------------------------------------
   this.html = null;
   this.count = 0;
   this.reactiveElements = [];
 
-  this.state = {
-    name: "My Template",
-    counter: 0,
+  //--------------------------------------------------
+  // Get Method
+  //--------------------------------------------------
+  this.get = key => {
+    return this.state[key];
   }
 
-  this.get = key => this.state[key];
-
+  //--------------------------------------------------
+  // Set Method
+  //--------------------------------------------------
+  // When updating values in this.state, use this
+  // method so that the app updates properly.
+  //--------------------------------------------------
+  // Example:
+  //--------------------------------------------------
+  // this.set("name", "New Name");
+  // Will update this.state.name = "New Name"
+  // and will update the change in the app
+  //--------------------------------------------------
   this.set = (key, val) => {
     this.state[key] = val;
     this.privateMethods.update(key);
   }
 
+  
+  //--------------------------------------------------
+  // Attribute Selectors
+  //--------------------------------------------------
+  // Define attribute selectors here. Use prefix "m-".
+  //--------------------------------------------------
   this.selectors = {
     id:     "m-id",
     class:  "m-class",
-    click:  "m-click"
+    click:  "m-click",
+    event:  "m-event"
   }
 
+  //--------------------------------------------------
+  // Data to be used in the app
+  //--------------------------------------------------
+  // Define your data here. It can be rendered in the
+  // app by referencing with {{<key>}}
+  // Example: {{name}} will output the value of
+  // this.state.name
+  //--------------------------------------------------
+  this.state = {
+    name: "My Template",
+    counter: 0,
+  }
+
+  //--------------------------------------------------
+  // HTML Template
+  //--------------------------------------------------
+  // Build your html here.
+  //--------------------------------------------------
+  // Attributes
+  //--------------------------------------------------
+  // Use m-class to set classes (multiple supported)
+  // Use m-event to define an event and handler
+  // separated by a comma.
+  //--------------------------------------------------
+  // Example:
+  //--------------------------------------------------
+  // m-event="<event name>,<method>"
+  // m-event="click,myMethod"
+  //--------------------------------------------------
   this.template = () => {
     let element = document.createElement("div");
         element.id = "container";
@@ -32,23 +87,36 @@ const Mane = () => {
           <p m-class="box blue">Class: box blue</p>
           <button
             m-class="my-button"
-            m-click="changeTitle"
+            m-event="click,changeTitle"
           >Change Title</button>
           <div m-class="counter-box">
             <p>{{counter}}</p>
             <button
               m-class="my-button"
-              m-click="counterUp"
+              m-event="click,counterUp"
             >Counter Up</button>
             <button
               m-class="my-button"
-              m-click="counterDown"
+              m-event="click,counterDown"
             >Counter Down</button>
           </div>
         `;
       return element;
   }
 
+  //--------------------------------------------------
+  // CSS
+  //--------------------------------------------------
+  // Define classes and hover states here.
+  // Single class selectors supported only.
+  // Nesting classes/elements not yet supported.
+  //--------------------------------------------------
+  // Example:
+  //--------------------------------------------------
+  // "<class name>": "<style>:<value>;"
+  // For hover states use:
+  // "<class name>:hover": "<style>:<value>;"
+  //--------------------------------------------------
   this.css = {
     "box": `
       border-width: 1px;
@@ -78,6 +146,12 @@ const Mane = () => {
     `
   }
 
+  //--------------------------------------------------
+  // Methods
+  //--------------------------------------------------
+  // Define methods to be used in the app here.
+  // Parameters not yet supported.
+  //--------------------------------------------------
   this.methods = ({
     changeTitle: () => {
       this.set("name", "My Template is awesome!");
@@ -90,6 +164,12 @@ const Mane = () => {
     }
   });
 
+  //--------------------------------------------------
+  // Private Methods
+  //--------------------------------------------------
+  // Required for the app to run.
+  // DO NOT TOUCH
+  //--------------------------------------------------
   this.privateMethods = ({
     buildHTML: () => {
       this.html = this.template();
@@ -138,8 +218,9 @@ const Mane = () => {
       });
     },
     assignMethods: () => {
-      document.querySelectorAll(`[${this.selectors.click}]`).forEach(element => {
-        element.addEventListener("click", this.methods[element.getAttribute(this.selectors.click)]);
+      document.querySelectorAll(`[${this.selectors.event}]`).forEach(element => {
+        let event = element.getAttribute(this.selectors.event).split(",");
+        element.addEventListener(event[0].trim(), this.methods[event[1].trim()]);
       });
     },
     createReactiveData: (parent) => {
